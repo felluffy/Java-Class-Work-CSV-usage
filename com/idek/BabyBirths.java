f419543995a835c76f5be49c075d7c968618b718
felluffy <somerandomghai@gmail.com>
2017-09-19 23:16:19 +0600

    add getRank method which gets the rank of a name if it is in a file of a given year if present otherwise which gets nothnig and testGetRank to test the former method

--- com/idek/BabyBirths.java
+++ com/idek/BabyBirths.java
@@ -30,15 +30,35 @@ public class BabyBirths
 		for(CSVRecord rec : fr.getCSVParser(false))
 		{
 			int numBorn = Integer.parseInt(rec.get(2));
+
 			if(rec.get(1).equals("M"))
 				maleBirths += numBorn;
 			else
 				femaleBirths += numBorn;
+				
+			//System.out.println("Number of babies named \'" +  rec.get(0) + "\' born are " + numBorn);
 		}
 		totalBirths = maleBirths + femaleBirths;
 		System.out.println("Total births: " + totalBirths);
 		System.out.println("Total male births: " + maleBirths);
 		System.out.println("Total female births: " + femaleBirths);
+	}
+	
+	//Returns the rank of the name of a name in a specific year if present
+	public int getRank(String name, String gender, int year)
+	{
+		String fileName = "data/yob" + String.valueOf(year) + "short.csv";
+		int rank = 0;
+		FileResource fr = new FileResource(fileName);
+		for(CSVRecord rec : fr.getCSVParser(false))
+		{
+			if(!rec.get(1).equals(gender))
+				continue;
+			rank++;
+			if(rec.get(0).equals(name))
+				return rank;
+		}
+		return -1;
 	}
 	
 	public void testTotalBirths()
@@ -47,8 +67,16 @@ public class BabyBirths
 		totalBirths(fr);
 	}
 	
+	public void testGetRank()
+	{
+		System.out.println(getRank("Mason", "M", 2012));
+		System.out.println(getRank("Mason", "F", 2012));
+		
+	}
+	
 	public void testClassMethods()
 	{
 		testTotalBirths();
+		testGetRank();
 	}
 }
